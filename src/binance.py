57a2d8e03ec78e717dd43d7994bdeac3fb74905c
sivefunc <sivefunc@tuta.io>
2024-10-30 21:28:49 -0400

    feat: exception handling

--- src/binance.py
+++ src/binance.py
@@ -3,6 +3,16 @@ from dataclasses import dataclass, field
 
 import requests
 
+class BinanceError(Exception):
+    """Binance json response related errors
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
 @dataclass
 class Binance():
     payload: Dict[str, str | int | list | bool] = field(
@@ -43,4 +53,16 @@ class Binance():
                 **kwargs)
 
         response.raise_for_status()
-        return float(response.json()['data'][0]['adv']['price'])
+        r_json = response.json()
+        if not r_json['success'] or not r_json['data']:
+            raise BinanceError(f"POST request Payload failed or not data\n"
+                                f"JSON response: {r_json}")
+
+        try:
+            price = float(response.json()['data'][0]['adv']['price'])
+
+        except Exception as error:
+            raise BinanceError(f"No price found on JSON or couldn't convert\n"
+                                f"JSON response: {r_json}") from error
+
+        return price
